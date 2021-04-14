47baf4c8ac40320601f6d1dc5cde4b7b611c54b9
Md Shahajahan Ali <msacse1@gmail.com>
2021-04-15 02:08:07 +0600

    Global and local variables & Accessing global variable inside the function

--- js/hello.js
+++ js/hello.js
@@ -1,3 +1,23 @@
-alert("Hello, World!, I am learning JavaScript");
+// alert("Hello, World!, I am learning JavaScript");
 var variables = "JavaScript Variables";
 console.log(variables);
+
+// global variable
+var message = "Hello";
+function say() {
+    // local variable
+    var message = 'Hi';
+    console.log(message); // which message?
+}
+say();// Hi
+console.log(message); // Hello
+
+// global variable
+var message1 = "Hello1";
+function says() {
+    // local variable
+    message1 = 'Hi1';
+    console.log(message1); // which message?
+}
+says();// Hi
+console.log(message1); // Hi
