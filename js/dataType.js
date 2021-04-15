1c86e1ae24bd05e7b72ac052f1750b08b986753b
Md Shahajahan Ali <msacse1@gmail.com>
2021-04-15 16:11:31 +0600

    Learn Data type

--- js/dataType.js
+++ js/dataType.js
@@ -1,4 +1,4 @@
-//  undefined data type
+/* //  undefined data type
 let undefine;
 console.log(undefine);        // undefined
 console.log(typeof undefine); // undefined
@@ -13,4 +13,53 @@ if (obj != null) {
     console.log('not ok');
 }
 
-console.log(null == undefined); // true
+console.log(null == undefined); // true */
+
+/* let oct = 060; // octal for 48
+console.log(oct);
+let d = 090; // 90
+console.log(d);
+
+console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
+console.log(Number.MIN_VALUE); // 5e-324
+console.log(Number.MAX_VALUE + Number.MAX_VALUE); // Infinity
+console.log(-Number.MAX_VALUE - Number.MAX_VALUE); // -Infinity
+console.log('a'/2); //NaN value
+
+let inProgress = true;
+let completed = false;
+console.log(typeof inProgress); // boolean
+ */
+
+console.log(Boolean('Hi'));// true
+console.log(Boolean(''));  // false
+
+console.log(Boolean(20));  // true
+console.log(Boolean(Infinity));  // true
+console.log(Boolean(0));  // false
+
+console.log(Boolean({foo: 100}));  // true on non-empty object
+console.log(Boolean(null));// false
+
+let s2 = Symbol("event.save");
+console.log(s2.toString()); // Symbol(event.save)
+
+const click = Symbol("click");
+console.log(click);
+
+let contact = {
+  firstName: "John",
+  lastName: "Doe",
+  email: "john.doe@example.com",
+  phone: "(408)-555-9999",
+  address: {
+    building: "4000",
+    street: "North 1st street",
+    city: "San Jose",
+    state: "CA",
+    country: "USA",
+  },
+};
+
+console.log(contact.firstName);
+console.log(contact.address.city);
