fe82b924c17975516027215b98c4a2fec737c2e7
Md Shahajahan Ali <msacse1@gmail.com>
2021-04-19 05:58:01 +0600

    How to use the JavaScript do-while statement to create a loop.

--- js/operators.js
+++ js/operators.js
@@ -102,3 +102,37 @@ while (counts < size) {
 
 console.log('Random array :' +rands);
 
+//  do while loop
+let coun = 0;
+do {
+  coun++;
+  console.log("count is :" + coun);
+} while (coun < 10);
+
+
+// generate secret number is a random integer between 1 and 12
+const MIN = 1;
+const MAX = 12;
+let secretNumber = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
+
+let guesses = 0; // for storing the number of guesses
+let hint = ''; // for storing hint
+let number = 0;
+do {
+    // get input from user
+    let input = prompt(`Please enter a number between ${MIN} and ${MAX}` + hint);
+    // get the integer
+    number = parseInt(input);
+    // increase the number of guesses
+    guesses++;
+    // check input number with the secret number
+    // provide hint if needed
+    if (number > secretNumber) {
+        hint = ', and less than ' + number;
+    } else if (number < secretNumber) {
+        hint = ', and greater than ' + number;
+    } else if(number == secretNumber) {
+        alert(`Bravo! you're correct after ${guesses} guess(es).`);
+    }
+} while (number != secretNumber);
+
