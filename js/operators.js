7c8c31acc5c02275f3cb0054bcc49a862fd4bd31
Md Shahajahan Ali <msacse1@gmail.com>
2021-04-22 18:07:28 +0600

    How to used breack, label statement & execution of code in a loop and how to exit a nested loop.

--- js/operators.js
+++ js/operators.js
@@ -136,3 +136,16 @@ do {
     }
 } while (number != secretNumber);
 
+
+// initialize j variable
+let j = 1;
+for (;;) {
+    // terminate the loop if j is greater than 10;
+    if (j > 10) {
+        break;
+    }
+    console.log(j);
+    // increase the counter j
+    j += 2;
+}
+
