ba9b73021d4be5c3f702a25374b23825b817201f
Md Shahajahan Ali <msacse1@gmail.com>
2021-04-23 23:55:44 +0600

    Learn about substr(), valueOf(), toString(), indexOf(), lastIndexOf()

--- js/string.js
+++ js/string.js
@@ -21,3 +21,29 @@ let className = "btn";
 className += " btn-primary";
 className += " none";
 console.log(className);
+
+let st = new String("JavaScript String Type");
+console.log(st.length); // 22
+console.log(st.valueOf());
+console.log(st.toString());
+console.log(st.toLocaleString());
+console.log(st.charAt(1)); // J
+
+// Concatenating strings by concat()
+let firstName = 'John';
+let fullName = firstName.concat(' Doe');
+console.log('concatenating by concat() : ' + fullName); // "John Doe"
+console.log(firstName); // "John"
+
+// Concatenating strings by operator (+)
+fullName = firstName + ' Doe';
+console.log('concatenating by operator (+) : ' + fullName); // "John Doe"
+
+// Extracting substrings
+let subst = "JavaScript String";
+console.log(subst.substr(0, 10)); // "JavaScript"
+console.log(subst.substr(11, 6)); // "String"
+
+let ts = "This is a string";
+console.log(ts.indexOf('is', 3)); //5
+console.log(ts.lastIndexOf("is")); // 5
