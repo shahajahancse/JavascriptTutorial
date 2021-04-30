85735918fba236e2d3bb39f1305168b1f01d97d8
Md Shahajahan Ali <msacse1@gmail.com>
2021-04-30 22:54:35 +0600

    Anonymous functions are functions without names.

--- js/functions.js
+++ js/functions.js
@@ -31,4 +31,37 @@ function add() {
 console.log('arguments sun : ' + add(1,3));
 console.log('arguments sun : ' + add(1,3, 5, 7));
 
-
+// Introduction to JavaScript anonymous functions
+let show = function () {
+    console.log('Anonymous function.');
+}
+show();
+// Using anonymous functions as arguments of other functions.
+setTimeout(function () {
+    console.log("Execute later after 1 second.");
+}, 1000);
+// Immediately invoked function execution.
+(function () {
+    console.log("Immediately");
+})();
+// First, the following defines a function expression:
+(function () {
+  console.log("Immediately invoked function execution");
+});
+// Second, the trailing parentheses () allow you to call the function:
+(function () {
+  console.log("Immediately invoked function execution");
+})();
+// and sometimes, want to pass arguments into it, like this:
+let person = {
+    firstName: 'John',
+    lastName: 'Due',
+};
+(function () {
+    console.log(`${person.firstName} ${person.lastName}`);
+})(person);
+// ES6 shorthand function.
+show = () => console.log("Anonymous functions");
+show();
+add = (a, b) => a + b;
+console.log(add(10, 15));
