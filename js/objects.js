d05166daccf9dbf760849f242d8769ef67298fd5
Md Shahajahan Ali <msacse1@gmail.com>
2021-05-01 17:47:39 +0600

    avaScript object’s properties and their attributes such as configurable, enumerable, writable, get, set, and value.

--- js/objects.js
+++ js/objects.js
@@ -82,3 +82,49 @@ person = {
 };
 console.log(person.getFullName());
 
+// used Object.defineProperty & Accessor properties
+person = {
+  firstName: "John",
+  lastName: "Doe",
+};
+Object.defineProperty(person, "fullName", {
+  get: function () {
+    return this.firstName + " " + this.lastName;
+  },
+  set: function (value) {
+    let parts = value.split(" ");
+    if (parts.length == 2) {
+      this.firstName = parts[0];
+      this.lastName = parts[1];
+    } else {
+      throw "Invalid name format";
+    }
+  },
+});
+console.log(person.fullName);
+// Define multiple properties: Object.defineProperties()
+var product = {};
+Object.defineProperties(product, {
+  name : {
+    value: 'SmartPhone',
+  },
+  price : {
+    value: 9000,
+  },
+  tax : {
+    value: 0.1,
+  },
+  netPrice : {
+    get : function () {
+      return this.price * (1 + this.tax);
+    },
+  },
+});
+console.log('The net price of a ' + product.name + ' is ' + product.netPrice.toFixed(2) + ' USD');
+// JavaScript object property descriptor
+person = {
+  firstName: "John",
+  lastName: "Doe",
+};
+let descriptor = Object.getOwnPropertyDescriptor(person, 'firstName');
+console.log(descriptor);
