ba9ce4ae48f64f6a34751b0a0c57ad7f1a3f9359
Md Shahajahan Ali <msacse1@gmail.com>
2021-05-04 22:52:54 +0600

    prototypal inheritance achieved via prototype linkage

--- js/constructorPrototype.js
+++ js/constructorPrototype.js
@@ -16,3 +16,30 @@ let person = {
   },
 };
 console.log(person instanceof Object);
+console.log(person.toString());
+console.log(person.__proto__);
+let teacher = {
+  teach: function (subject) {
+    return "I can teach " + subject;
+  },
+};
+teacher.__proto__ = person;
+console.log(teacher.name);
+console.log(teacher.greet());
+// A standard way to implement prototypal inheritance in ES5
+teacher = Object.create(person);
+teacher.name = "Jane Doe";
+teacher.teach = function (subject) {
+  return "I can teach " + subject;
+};
+// Or
+// let teacher = Object.create(person, {
+//   name: { value: "John Doe" },
+//   teach: {
+//     value: function (subject) {
+//       return "I can teach " + subject;
+//     },
+//   },
+// });
+console.log(Object.getPrototypeOf(teacher) === person);
+
