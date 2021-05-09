8ccc8c5a6e4ca8c8cc7fdeef0aab89f693ba74c4
Md Shahajahan Ali <msacse1@gmail.com>
2021-05-09 19:11:18 +0600

    A function also has three important methods: call(), apply(), and bind().

--- js/advancedFunction.js
+++ js/advancedFunction.js
@@ -61,4 +61,52 @@ let firstNames = name.firstName,
 let { firstNamess, lastNamess } = getName();
 console.log(firstNames);
 
+// JavaScript Function Type
+function swap(x, y) {
+    x = x + y;
+    y = x - y;
+    x = x - y;
+    return "x : " +x+" Y : "+ y;
+}
+console.log(swap(10, 5));
+console.log(swap.length); // 2
+console.log(swap.prototype); // Object{}
+// ES6
+function fn() {
+    console.log(new.target);
+}
+fn(); // undefined
+let f = new fn(); // [Function: fn]
+// The apply() and call() methods call a function with a given this value and arguments.
+let cat = {type: "Cat", sound: "Meow"};
+let dog = {type: "Dog", sound: "woof"};
+let say = function (greeting) {
+  console.log(greeting);
+  console.log(this.type +" say "+ this.sound);
+};
+say.apply(cat, ['Hi']);
+// Hi  // Cat says Meow
+say.apply(dog,['Hi']);
+// Hi  // Dog says Woof
+say.call(cat,'Hi');
+// Hi  // Cat says Meow
+say.call(dog,'Hi');
+// Hi  // Dog says Woof
+// The bind() method creates a new function instance whose this value is bound to the object that you provide.
+let car = {
+  speed: 5,
+  start: function () {
+    console.log('Start with ' +this.speed+ ' km/h');
+  }
+};
+let aircraft = {
+  speed: 10,
+  fly: function () {
+    console.log("Flying");
+  }
+};
+car.start.call(aircraft);
+let taxing = car.start.bind(aircraft);
+taxing();
+
 
