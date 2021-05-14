401863aa42d7e86a8c7ba702969c3802a1ea9b07
Md Shahajahan Ali <msacse1@gmail.com>
2021-05-15 01:48:06 +0600

    avaScript literal templates and how to apply them in your code.

--- es6/varLet.js
+++ es6/varLet.js
@@ -16,3 +16,32 @@ console.log(window.lcount); //undefined
 // Re-declaration
 var vcount = 10;
 //let lcount = 10; //// Uncaught SyntaxError: Identifier 'lcount' has already been declared
+
+//The basic syntax of JavaScript template literals
+let str = 'Template literal in ES6.';
+console.log(str);
+console.log(str.length); //24
+console.log(typeof str); //string
+let anotherStr = `Here's a template literal`;
+console.log(anotherStr);
+let strWithBacktick = `Template literals use backticks \` insead of quotes`;
+console.log(strWithBacktick);
+// Multiline strings
+var msg = 'multiline \n\ string';
+console.log(msg);
+var p = `This text
+can
+span multiple lines`;
+console.log(p);
+let post = {
+    title: 'JavaScript Template Literals',
+    excerpt: 'Introduction to JavaScript Template Literals in ES6',
+    body: 'Content of the post will be here....',
+    tags: ['es6', 'template literals', 'JavaScript'],
+};
+
+
+
+
+
+
