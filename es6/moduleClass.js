616f579da0486f0132d79818be3e94806538f294
Md Shahajahan Ali <msacse1@gmail.com>
2021-05-16 22:44:17 +0600

    Use JavaScript class keyword declares a new class.

--- es6/moduleClass.js
+++ es6/moduleClass.js
@@ -20,4 +20,30 @@ console.log(total(20, 30));
 // export { sum as add } from "./math.js";
 
 
+// Classes to ES6 // Classes prior to ES6 revisited
+function Person(name) {
+    this.name = name;
+}
+Person.prototype.getName = function () {
+    return this.name;
+}
+var john = new Person('John Due');
+console.log(john.getName());
+console.log(john instanceof Person); // true
+console.log(john instanceof Object); // true
+/* ES6 class declaration */
+class Persons {
+    constructor(name) {
+        this.name = name;
+    }
+    getName() {
+        return this.name;
+    }
+}
+john = new Persons('John Due');
+let name = john.getName();
+console.log(name); // "John Doe"
+console.log(typeof Persons); // function
+console.log(john instanceof Persons); // true
+console.log(john instanceof Object); // true
 
