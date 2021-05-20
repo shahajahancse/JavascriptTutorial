cebf3045a4d0d6f3aad40fd48846b9eaed001900
Md Shahajahan Ali <msacse1@gmail.com>
2021-05-21 03:53:11 +0600

    JavaScript Generators and how to use them effectively.

--- es6/iteratorGenerator.js
+++ es6/iteratorGenerator.js
@@ -44,3 +44,93 @@ for (const num of oddNumbers) {
     }
     console.log(num);
 }
+
+
+// Introduction to JavaScript Generators
+function* generate() {
+    console.log('invoked 1st time');
+    yield 1;
+    console.log('invoked 2nd time');
+    yield 2;
+}
+let gen = generate();
+console.log(gen);
+result = gen.next();
+console.log(result);
+result = gen.next();
+console.log(result);
+result = gen.next();
+console.log(result);
+for (const g of gen) {
+    console.log(g);
+}
+// More generator examples.
+function* forever() {
+    let index = 0;
+    while (true) {
+        yield index++;
+    }
+}
+let f = forever();
+console.log(f.next()); // 0
+console.log(f.next()); // 1
+console.log(f.next()); // 2
+// Using generators to implement iterators
+class Sequences {
+  constructor( start = 0, end = Infinity, interval = 1 ) {
+    this.start = start;
+    this.end = end;
+    this.interval = interval;
+  }
+  // [Symbol.iterator]() {
+  //   let counter = 0;
+  //   let nextIndex = this.start;
+  //   return {
+  //     next: () => {
+  //       if ( nextIndex <= this.Infinity ) {
+  //         let result = { value: nextIndex, done: false };
+  //         nextIndex += this.interval;
+  //         counter ++;
+  //         return result;
+  //       }
+  //       return { value: counter, done: true };
+  //     }
+  //   }
+  // }
+  * [Symbol.iterator]() {
+    for( let index = this.start; index <= this.end; index += this.interval ) {
+        yield index;
+    }
+  }
+}
+oddNumbers = new Sequences(1, 10, 2);
+for (const num of oddNumbers) {
+  console.log(num);
+}
+// Using a generator to implement the Bag data structure
+class Bag {
+    constructor() {
+        this.elements = [];
+    }
+    isEmpty() {
+        return this.elements.length === 0;
+    }
+    add(element) {
+        this.elements.push(element);
+    }
+    * [Symbol.iterator]() {
+        for (let element of this.elements) {
+            yield element;
+        }
+    }
+}
+
+let bag = new Bag();
+
+bag.add(1);
+bag.add(2);
+bag.add(3);
+
+for (let e of bag) {
+    console.log(e);
+}
