9afaf8258cee050ddf908aa6c9026f127d5ac02d
Md Shahajahan Ali <msacse1@gmail.com>
2021-05-23 12:48:48 +0600

    Resolved the promises.

--- es6/promises.js
+++ es6/promises.js
@@ -89,3 +89,23 @@ function getServiceCost(services) {
     });
 }
 getUser(100).then(getServices).then(getServiceCost).then(console.log);
+
+
+//  Resolved promises example
+const p1 = new Promise((resolve, reject) => {
+    console.log('The first promise has resolved');
+    resolve(10);
+});
+const p2 = new Promise((resolve, reject) => {
+    console.log('The second promise has resolved');
+    resolve(20);
+});
+const p3 = new Promise((resolve, reject) => {
+    console.log('The third promise has resolved');
+    resolve(30);
+});
+Promise.all([p1, p2, p3]).then(results => {
+    const total = results.reduce((p, c) => p + c);
+    console.log(`Results: ${results}`);
+    console.log(`Total: ${total}`);
+});
