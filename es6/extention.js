fe6f04d86edf14dd8679fa6b3c3d7f055059d00e
Md Shahajahan Ali <msacse1@gmail.com>
2021-05-24 22:44:58 +0600

    JavaScript Array’s find() method to search for the first occurrence of an element that satisfies a test.

--- es6/extention.js
+++ es6/extention.js
@@ -61,3 +61,20 @@ let even = {
 };
 let evenNumbers = Array.from(even);
 console.log(evenNumbers);
+
+
+// Introduction to the Array find() method // JavaScript find() examples
+numbers = [1, 2, 3, 4, 5];
+console.log(numbers.find((e) => e % 2 == 0));
+let customers = [{
+    name: 'ABC Inc',
+    credit: 100
+}, {
+    name: 'ACME Corp',
+    credit: 200
+}, {
+    name: 'IoT AG',
+    credit: 300
+}];
+console.log(customers.find((c) => c.credit > 100));
+
