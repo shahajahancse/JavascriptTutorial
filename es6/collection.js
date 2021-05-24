ed85beaf348015af9143fb4f2c5ed9435a4409e4
Md Shahajahan Ali <msacse1@gmail.com>
2021-05-24 22:12:22 +0600

    JavaScript Map object and its useful methods to manipulate entries in the map.

--- es6/collection.js
+++ es6/collection.js
@@ -46,6 +46,70 @@ if (equipment.has(server)) {
 }
 
 
+// Introduction to JavaScript Map object
+let john = { name: "John Doe" },
+  lily = { name: "Lily Bush" },
+  peter = { name: "Peter Drucker" };
+let userRoles = new Map();
+console.log(typeof(userRoles)); // object
+console.log(userRoles instanceof Map); // true
+// Add elements to a Map
+userRoles.set(john, "admin");
+userRoles.set(lily, 'editor')
+          .set(peter, 'subscriber');
+// Initialize a map with an iterable object
+userRoles = new Map([
+  [john, "admin"],
+  [lily, "editor"],
+  [peter, "subscriber"],
+]);
+// Get an element from a map by key
+console.log(userRoles.get(john)); // admin
+let foo = {name: 'Foo'};
+console.log(userRoles.get(foo)); //undefined
+// Check the existence of an element by key
+console.log(userRoles.has(foo)); // false
+console.log(userRoles.has(lily)); // true
+// Get the number of elements in the map
+console.log(userRoles.size); // 3
+// Iterate over map keys
+for (let user of userRoles.keys()) {
+    console.log(user.name);
+}
+// John Doe
+// Lily Bush
+// Peter Drucker
+// Iterate over map values
+for (let role of userRoles.values()) {
+    console.log(role);
+}
+// admin
+// editor
+// subscriber
+// Iterate over map elements
+for (let elem of userRoles.entries()) {
+    console.log(`${elem[0].name}: ${elem[1]}`);
+}
+// John Doe: admin
+// Lily Bush: editor
+// Peter Drucker: subscriber
+// or
+for (let [user, role] of userRoles.entries()) {
+  console.log(`${user.name}: ${role}`);
+}
+// or
+userRoles.forEach((role, user) =>
+    console.log(`${user.name}: ${role}`)
+);
+// Convert map keys or values to a array
+var keys = [...userRoles.keys()];
+console.log(keys);
 
-
-
+roles = [...userRoles.values()];
+console.log(roles);
+// Delete an element by key
+userRoles.delete(john);
+console.log(userRoles.size); // 0
+// Delete all elements in the map
+userRoles.clear();
+console.log(userRoles.size); // 0
