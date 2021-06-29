bec6678b38c0f00e734828fef224aa146720ca91
Md Shahajahan Ali <msacse1@gmail.com>
2021-06-29 20:48:35 +0600

    learn for ... of loop

--- second/js/expressions.js
+++ second/js/expressions.js
@@ -85,3 +85,96 @@ result = str.match(re);
 console.log(result);
 
 
+function sum(...args) {
+    let total = 0;
+    for (const a of args) {
+        total += a;
+    }
+    return total;
+}
+console.log(sum(1, 2, 3));
+
+const combine = (...args) => {
+	return args.reduce(function (prev, curr) {
+		return prev + " " + curr;
+	});
+};
+message = combine("JavaScript", "Rest", "Parameters");
+console.log(message); // JavaScript Rest Parameters
+
+var rivers = ["Nile", "Ganges", "Yangtze"];
+var moreRivers = ["Danube", "Amazon"];
+Array.prototype.push.apply(rivers, moreRivers);
+console.log(rivers);
+
+let initialChars = ['A', 'B'];
+let chars = [...initialChars, 'C', 'D'];
+console.log(chars); // ["A", "B", "C", "D"]
+
+let prefix = 'machine';
+let machine = {
+    [prefix + ' name']: 'server',
+    [prefix + ' hours']: 10000
+};
+console.log(machine['machine name']); // server
+console.log(machine['machine hours']); // 10000
+
+let server = {
+    name: 'Server',
+    restart() {
+        console.log("The " + this.name + " is restarting...");
+    },
+    'starting up'() {
+        console.log("The " +  this.name + " is starting up!");
+    }
+};
+server['starting up']();
+server.restart();
+
+
+// JavaScript for of loop examples
+let scores = [80, 90, 70];
+for (let score of scores) {
+    score = score + 5;
+    console.log(score);
+}
+let colors = ['Red', 'Green', 'Blue'];
+for (let color of colors) {
+    console.log(color);
+}
+for (let [index, color] of colors.entries()) {
+  console.log(`${index + " " + color}`);
+}
+
+// 2) In-place object destructuring with for…of
+const ratings = [
+    {user: 'John',score: 3},
+    {user: 'Jane',score: 4},
+    {user: 'David',score: 5},
+    {user: 'Peter',score: 2},
+];
+for (const row of ratings) {
+    console.log(row.user +" "+ row.score);
+}
+// with index
+for (const [index,row] of ratings.entries()) {
+  console.log(`${index+1 +" "+ row.user + " " + row.score}`);
+}
+// 3) Iterating over strings
+str = 'abc';
+for (let c of str) {
+    console.log(c);
+}
+// 3) Iterating over Map objects
+colors = new Map();
+colors.set('red', '#ff0000');
+colors.set('green', '#00ff00');
+colors.set('blue', '#0000ff');
+for (let color of colors) {
+    console.log(color);
+}
+// 4) Iterating over Set objects
+let nums = new Set([1, 2, 3]);
+for (let num of nums) {
+    console.log(num);
+}
